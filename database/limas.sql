06fc68914792d3ec41ecf75b7717c37126f574e7
Luiz Domingues <luiz.f.l.domingues@gmail.com>
2025-04-21 17:03:50 -0300

    Add the status column in the database for the product_types and products table

--- database/limas.sql
+++ database/limas.sql
@@ -1,6 +1,7 @@
 CREATE TABLE product_types (
     id INTEGER PRIMARY KEY NOT NULL,
-    product_type TEXT NOT NULL
+    product_type TEXT NOT NULL,
+    type_status TEXT CHECK (type_status IN ('active', 'unactive')) NOT NULL DEFAULT 'active'
 );
 
 CREATE TABLE products (
@@ -8,6 +9,7 @@ CREATE TABLE products (
     product_name TEXT NOT NULL,
     product_type_id INTEGER NOT NULL,
     price INTEGER NOT NULL,
+    product_status TEXT CHECK (product_status IN ('active', 'unactive')) NOT NULL DEFAULT 'active',
     FOREIGN KEY (product_type_id) REFERENCES product_types(id)
 );
 
@@ -47,4 +49,3 @@ CREATE TABLE order_products (
     FOREIGN KEY (product_id) REFERENCES products(id),
     FOREIGN KEY (order_increment_id) REFERENCES order_increments(id)
 );
-

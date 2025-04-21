3c4381ec205a123b7a8f508733d3591360d641aa
Luiz Domingues <luiz.f.l.domingues@gmail.com>
2025-04-21 09:42:20 -0300

    Create new folder database and move all the database related files to it

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
 # Configure the cs50 lib to use the database
-db = SQL("sqlite:///limas.db")
+db = SQL("sqlite:///database/limas.db")
 
 # TODO: understand what this means
 @app.after_request

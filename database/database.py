da0ab1aa5eb51bc7da2f0f726e1acc110a82f2b2
Luiz Domingues <luiz.f.l.domingues@gmail.com>
2025-07-22 10:46:05 -0300

    database: automatically creates the database when its file does not exist

---
+++ database/database.py
@@ -0,0 +1,20 @@
+import sqlite3
+import os
+
+class Database:
+    def __init__(self, db_path):
+        self.db_path = db_path
+        def new_database():
+            with open('database/limas.sql', 'r') as sql_file:
+                sql_script = sql_file.read()
+                print(sql_script)
+                with sqlite3.connect(self.db_path) as con:
+                    cur = con.cursor()
+                    cur.executescript(sql_script)
+                    con.commit()
+                    print("Novo banco de dados criado com sucesso")
+        if not os.path.isfile(self.db_path):
+            new_database()
+
+Database("database/limas.db")
+
